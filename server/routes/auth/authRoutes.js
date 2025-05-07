e0feb4f15b3b8d72ba7373228cf3ec6fecf16b08
Muhammad Abdullah <ma3472995@gmail.com>
2025-05-07 16:38:35 +0500

    updated

--- server/routes/auth/authRoutes.js
+++ server/routes/auth/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../../controllers/auth/authController');
+const { signup, login, verifyUser, resetPassword } = require('../../controllers/auth/authController');
 
 router.post('/signup', signup);
 router.post('/login', login);
-
+// routes/auth.js
+router.post("/verify", verifyUser);
+router.post("/reset-password", resetPassword);
 module.exports = router;
